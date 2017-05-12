1c84802a11dcbafa23111bef848da0bed37d68c7
mlauderdale <mlauderdale@autotrader.com>
2017-05-12 15:55:29 -0400

    setting up data

--- src/Component.jsx
+++ src/Component.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import fetchEndPoint from './components/js/util/fetchEndPoint';
+import { createStore } from 'redux';
 require('./components/scss/util/_default');
 
-export default class Component extends React.Component {
+class Component extends React.Component {
+  componentWillMount() {
+    console.log('component about to mount');
+    const reducer = (initialState = 0, action) => {
+      if (action.type === 'INC') {
+        return action.payload;
+      }
+    };
+
+    const store = createStore(reducer, 1);
+
+    store.subscribe(() => {
+      console.log('store changed', store.getState());
+    });
+    const dispatchData = (data)=> {
+      store.dispatch({type: 'INC', payload: data});
+    };
+
+    fetchEndPoint('/endpoint/promo-model.json', dispatchData);
+
+  }
   render() {
+
     if (this.props.onRender) {
       this.props.onRender();
     }
@@ -16,3 +39,5 @@ export default class Component extends React.Component {
 Component.propTypes = {
   onRender: React.PropTypes.func
 };
+
+export default Component;
